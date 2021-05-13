5bf2df54c8fd9c9285a7549ada92fdd22f0c441c
apollon313131 <apollo-bel@mail.ru>
2021-05-13 22:11:45 +0300

    step 1 - template

---
+++ src/components/Body.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Body() {
+    return (
+        <>
+        <h1 > Урок №2 </h1>
+        <h4> Продвинутый React</h4>
+
+        </>
+    )
+}
+
+export default Body
